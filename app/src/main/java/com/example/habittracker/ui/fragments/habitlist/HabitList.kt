7185e1acc1a6b0a28eb6ee2770965811014268ea
jajendrapandeya <=>
2020-09-22 14:15:06 +0545

    Created functionality for adding habits

--- app/src/main/java/com/example/habittracker/ui/fragments/habitlist/HabitList.kt
+++ app/src/main/java/com/example/habittracker/ui/fragments/habitlist/HabitList.kt
@@ -3,13 +3,18 @@ package com.example.habittracker.ui.fragments.habitlist
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.habittracker.R
+import kotlinx.android.synthetic.main.fragment_habit_list.*
 
 
 class HabitList : Fragment(R.layout.fragment_habit_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+        fab_add.setOnClickListener {
+            findNavController().navigate(R.id.action_habitList_to_createHabitItem)
+        }
     }
 
 }
